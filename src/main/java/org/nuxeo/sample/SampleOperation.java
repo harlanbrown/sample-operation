668ac122a5e8c8213ed6ccb1e89c2e43e2285f87
Harlan Brown <harlan.brown@hyland.com>
2023-04-04 19:02:51 -0500

    audit

--- src/main/java/org/nuxeo/sample/SampleOperation.java
+++ src/main/java/org/nuxeo/sample/SampleOperation.java
@@ -1,10 +1,26 @@
 package org.nuxeo.sample;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.Serializable;
+import java.io.Writer;
 import java.util.List;
+import java.util.Map;
+import java.nio.file.Files;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVPrinter;
+import org.apache.commons.csv.CSVRecord;
 import org.nuxeo.ecm.automation.core.Constants;
 import org.nuxeo.ecm.automation.core.annotations.Context;
 import org.nuxeo.ecm.automation.core.annotations.Operation;
 import org.nuxeo.ecm.automation.core.annotations.OperationMethod;
+import org.nuxeo.ecm.core.api.Blob;
+import org.nuxeo.ecm.core.api.Blobs;
 import org.nuxeo.ecm.core.api.CoreSession;
 import org.nuxeo.ecm.core.api.DocumentModel;
 import org.nuxeo.ecm.core.query.sql.model.Predicates;
@@ -21,17 +37,39 @@ public class SampleOperation {
     protected CoreSession session;
 
     @OperationMethod
-    public DocumentModel run(DocumentModel input){
+    public Blob run() throws IOException {
 
         AuditReader reader = Framework.getService(AuditReader.class);
         AuditQueryBuilder builder = new AuditQueryBuilder();        
         builder.predicate(Predicates.eq("repositoryId", "default"))
-            .and(Predicates.eq("eventId", "download"))
-            .and(Predicates.eq("extended.blobXPath", "file:content"))
-            .and(Predicates.noteq("extended.downloadReason", "preview"));
+            .and(Predicates.eq("eventId", "download"));
+//            .and(Predicates.eq("extended.blobXPath", "file:content"))
+//            .and(Predicates.noteq("extended.downloadReason", "preview"));
 
-//        List<LogEntry> logEntriesFiltered = reader.queryLogs(builder);
+        List<LogEntry> logEntriesFiltered = reader.queryLogs(builder);
 
-        return input;
+        CSVFormat csvFormat = CSVFormat.DEFAULT.withHeader("id", "type", "date", "comment").withRecordSeparator('\n');
+
+        File outputFile = Framework.createTempFile("report",".csv");
+
+        if (outputFile != null) {
+            Writer writer = new BufferedWriter(new FileWriter(outputFile));
+            CSVPrinter csvPrinter = new CSVPrinter(writer, csvFormat);
+
+            logEntriesFiltered.forEach(entry -> { 
+                try {
+                    csvPrinter.printRecord(entry.getDocUUID(), entry.getDocType(), entry.getEventDate().toString(), entry.getComment()); 
+                } catch (IOException e) {
+                }
+            });
+
+            csvPrinter.flush();
+            csvPrinter.close();
+        } 
+        Blob result = Blobs.createBlob(new FileInputStream(outputFile));        
+        result.setFilename("report.csv");        
+        Files.delete(outputFile.toPath());
+
+        return result;
     }
 }
--- src/test/java/org/nuxeo/sample/TestSampleOperation.java
+++ src/test/java/org/nuxeo/sample/TestSampleOperation.java
@@ -1,5 +1,10 @@
 package org.nuxeo.sample;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import javax.inject.Inject;
 import org.junit.Before;
 import org.junit.Test;
@@ -12,6 +17,8 @@ import org.nuxeo.ecm.core.api.CoreInstance;
 import org.nuxeo.ecm.core.api.CoreSession;
 import org.nuxeo.ecm.core.api.DocumentModel;
 import org.nuxeo.ecm.core.api.PathRef;
+import org.nuxeo.ecm.core.api.impl.blob.FileBlob;
+import org.nuxeo.ecm.core.api.Blob;
 import org.nuxeo.ecm.core.api.security.ACE;
 import org.nuxeo.ecm.core.api.security.ACL;
 import org.nuxeo.ecm.core.api.security.ACP;
@@ -19,15 +26,19 @@ import org.nuxeo.ecm.core.api.security.SecurityConstants;
 import org.nuxeo.ecm.core.test.DefaultRepositoryInit;
 import org.nuxeo.ecm.core.test.annotations.Granularity;
 import org.nuxeo.ecm.core.test.annotations.RepositoryConfig;
+import org.nuxeo.ecm.platform.audit.AuditFeature;
+import org.nuxeo.ecm.platform.audit.api.AuditLogger;
+import org.nuxeo.ecm.platform.audit.api.AuditReader;
+import org.nuxeo.ecm.platform.audit.api.LogEntry;
+import org.nuxeo.ecm.platform.audit.impl.LogEntryImpl;
+import org.nuxeo.runtime.api.Framework;
 import org.nuxeo.runtime.test.runner.Deploy;
 import org.nuxeo.runtime.test.runner.Features;
 import org.nuxeo.runtime.test.runner.FeaturesRunner;
-import java.util.HashMap;
-import java.util.Map;
-import org.nuxeo.ecm.platform.audit.AuditFeature;
+
 
 @RunWith(FeaturesRunner.class)
-@Features({ AuditFeature.class })
+@Features(AuditFeature.class)
 @RepositoryConfig(init = DefaultRepositoryInit.class, cleanup = Granularity.METHOD)
 @Deploy("org.nuxeo.ecm.automation.core")
 @Deploy("org.nuxeo.ecm.automation.features")
@@ -49,14 +60,25 @@ public class TestSampleOperation {
         fil1 = session.createDocument(fil1);
 
         session.save();
+
+        AuditLogger auditLogger = Framework.getService(AuditLogger.class);
+        List<LogEntry> newEntries = new ArrayList<>();
+
+        LogEntry entry = new LogEntryImpl();
+        entry.setCategory("somecat");
+        entry.setEventId("someEvent");
+        entry.setEventDate(new Date());
+        entry.setPrincipalName("toto");
+
+        newEntries.add(entry);
+        auditLogger.addLogEntries(newEntries);
+
     }
 
     @Test
     public void shouldCallOperation() throws OperationException {
         OperationContext ctx = new OperationContext(session);
         Map<String, Object> params = new HashMap<>();
-        DocumentModel input = session.getDocument(new PathRef("/default-domain/workspaces/ws1/fil1"));
-        ctx.setInput(input);
-        DocumentModel doc = (DocumentModel) automationService.run(ctx, SampleOperation.ID, params);
+        Blob b = (FileBlob) automationService.run(ctx, SampleOperation.ID, params);
     }
 }
