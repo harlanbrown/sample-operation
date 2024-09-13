beb3775a0b4288b9fb826c729a18ff2ca5b2ec53
harlan.brown@hyland.com <harlan.brown@hyland.com>
2024-09-13 13:55:31 -0500

    delete versions

--- src/main/java/org/nuxeo/sample/SampleOperation.java
+++ src/main/java/org/nuxeo/sample/SampleOperation.java
@@ -1,32 +1,56 @@
 package org.nuxeo.sample;
 
-import java.util.Map;
-import java.util.Map.Entry;
 import org.nuxeo.ecm.automation.core.Constants;
 import org.nuxeo.ecm.automation.core.annotations.Context;
 import org.nuxeo.ecm.automation.core.annotations.Operation;
 import org.nuxeo.ecm.automation.core.annotations.OperationMethod;
-import org.nuxeo.ecm.core.api.Blob;
 import org.nuxeo.ecm.core.api.CoreSession;
 import org.nuxeo.ecm.core.api.DocumentModel;
-import org.nuxeo.ecm.core.blob.BlobInfo;
-import org.nuxeo.ecm.core.blob.BlobManager;
-import org.nuxeo.ecm.core.blob.BlobProvider;
-import org.nuxeo.ecm.core.blob.SimpleManagedBlob;
-import org.nuxeo.ecm.platform.mimetype.MimetypeNotFoundException;
-import org.nuxeo.ecm.platform.mimetype.interfaces.MimetypeRegistry;
+import org.nuxeo.ecm.core.api.DocumentRef;
 import org.nuxeo.runtime.api.Framework;
 
 @Operation(id=SampleOperation.ID, category=Constants.CAT_DOCUMENT, label="SampleOperation", description="sample operation")
 public class SampleOperation {
+
     public static final String ID = "Document.SampleOperation";
+
+    private static int MAX_ITERATION = 5;
 
     @Context
     protected CoreSession session;
 
     @OperationMethod
     public DocumentModel run(DocumentModel input){
-        return input;
+
+        DocumentModel doc = session.getSourceDocument(input.getRef()); 
+        for (int i = 0; i < MAX_ITERATION && !isLive(doc); i++) {
+            doc = session.getSourceDocument(doc.getRef());
+        }
+
+        if (!session.isCheckedOut(doc.getRef())) {
+            session.checkOut(doc.getRef());
+        }
+        session.removeDocument(input.getRef());
+        return null;
+    }
+
+    @OperationMethod
+    public DocumentModel run(DocumentRef input){
+
+        DocumentModel doc = session.getSourceDocument(input);
+        for (int i = 0; i < MAX_ITERATION && !isLive(doc); i++) {
+            doc = session.getSourceDocument(doc.getRef());
+        }
+
+        if (!session.isCheckedOut(doc.getRef())) {
+            session.checkOut(doc.getRef());
+        }
+        session.removeDocument(input);
+        return null;
+    }
+
+    private boolean isLive(DocumentModel doc) {
+        return !doc.isVersion() && !doc.isProxy();
     }
 
 }
