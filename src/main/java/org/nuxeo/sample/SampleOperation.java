c34fae7ee3867b544e16359339cebf6b5ae04e57
harlan.brown@hyland.com <harlan.brown@hyland.com>
2026-08-13 13:59:34 -0500

    2025 SampleOperation

--- src/main/java/org/nuxeo/sample/SampleOperation.java
+++ src/main/java/org/nuxeo/sample/SampleOperation.java
@@ -1,31 +1,27 @@
 package org.nuxeo.sample;
 
-import java.util.Map;
-import java.util.Map.Entry;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.nuxeo.ecm.automation.core.Constants;
 import org.nuxeo.ecm.automation.core.annotations.Context;
 import org.nuxeo.ecm.automation.core.annotations.Operation;
 import org.nuxeo.ecm.automation.core.annotations.OperationMethod;
-import org.nuxeo.ecm.core.api.Blob;
 import org.nuxeo.ecm.core.api.CoreSession;
 import org.nuxeo.ecm.core.api.DocumentModel;
-import org.nuxeo.ecm.core.blob.BlobInfo;
-import org.nuxeo.ecm.core.blob.BlobManager;
-import org.nuxeo.ecm.core.blob.BlobProvider;
-import org.nuxeo.ecm.core.blob.SimpleManagedBlob;
-import org.nuxeo.ecm.platform.mimetype.MimetypeNotFoundException;
-import org.nuxeo.ecm.platform.mimetype.interfaces.MimetypeRegistry;
 import org.nuxeo.runtime.api.Framework;
 
 @Operation(id=SampleOperation.ID, category=Constants.CAT_DOCUMENT, label="SampleOperation", description="sample operation")
 public class SampleOperation {
     public static final String ID = "Document.SampleOperation";
 
+    protected static final Log log = LogFactory.getLog(SampleOperation.class);
+
     @Context
     protected CoreSession session;
 
     @OperationMethod
     public DocumentModel run(DocumentModel input){
+        log.info("HI from SampleOperation");
         return input;
     }
 
